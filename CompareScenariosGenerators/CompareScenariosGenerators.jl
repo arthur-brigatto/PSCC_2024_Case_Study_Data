4db8321a3dab8e56e1b92b34f5b321c0bfdd0459
arthur-brigatto <arthur.brigatto@gmail.com>
2023-10-06 15:58:56 -0300

    Adjust file names

--- CompareScenariosGenerators/CompareScenariosGenerators.jl
+++ CompareScenariosGenerators/CompareScenariosGenerators.jl
@@ -1,58 +1,55 @@
-using Pkg
-Pkg.activate(raw"C:\Users\brigatto\.julia\packages\CompareScenariosGenerators\js7fG")
-
 using CompareScenariosGenerators
 
 # GAS
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_GAS\evaluation_config.toml"])
-#
-## NW PARp
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_NW_PARp\evaluation_config.toml"])
-#
-## NW PARp-A
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_NW_PARpA\evaluation_config.toml"])
-#
-## PARp-A with J = 20
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_20\evaluation_config.toml"])
-#
-## PARp-A with J = 30
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_30\evaluation_config.toml"])
-#
-## PARp-A with J = 40
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_40\evaluation_config.toml"])
-#
-## PARp-A with J = 50
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_50\evaluation_config.toml"])
-#
-## PARp-A with J = 60
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_60\evaluation_config.toml"])
-#
-## PARp-A with J = 70
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_70\evaluation_config.toml"])
-#
-## PARp-A with J = 80
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_80\evaluation_config.toml"])
-#
-## PARp-A with R = 1
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_R_1\evaluation_config.toml"])
-#
-## PARp-A with R = 3
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_R_3\evaluation_config.toml"])
-#
-## PARp-A with R = 10
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_R_10\evaluation_config.toml"])
-#
-## SARIMA
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_SARIMA\evaluation_config.toml"])
-#
-## SeasonalNaive
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_SeasonalNaive\evaluation_config.toml"])
-#
-## Structural Model
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_StructuralModel\evaluation_config.toml"])
-#
-## Monthly median
-#CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_MonthlyMedian\evaluation_config.toml"])
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_GAS\evaluation_config.toml"])
+
+# NW PARp
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_NW_PARp\evaluation_config.toml"])
+
+# NW PARp-A
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_NW_PARpA\evaluation_config.toml"])
+
+# PARp-A with J = 20
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_20\evaluation_config.toml"])
+
+# PARp-A with J = 30
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_30\evaluation_config.toml"])
+
+# PARp-A with J = 40
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_40\evaluation_config.toml"])
+
+# PARp-A with J = 50
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_50\evaluation_config.toml"])
+
+# PARp-A with J = 60
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_60\evaluation_config.toml"])
+
+# PARp-A with J = 70
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_70\evaluation_config.toml"])
+
+# PARp-A with J = 80
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_J_80\evaluation_config.toml"])
+
+# PARp-A with R = 1
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_R_1\evaluation_config.toml"])
+
+# PARp-A with R = 3
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_R_3\evaluation_config.toml"])
+
+# PARp-A with R = 10
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_PARp-A_R_10\evaluation_config.toml"])
+
+# SARIMA
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_SARIMA\evaluation_config.toml"])
+
+# SeasonalNaive
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_SeasonalNaive\evaluation_config.toml"])
+
+# Structural Model
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_StructuralModel\evaluation_config.toml"])
+
+# Monthly median
+CompareScenariosGenerators.main_evaluation_loop([raw"C:\Users\brigatto\Documents\GitHub\PSCC_2024_Case_Study_Data\CompareScenariosGenerators\model_MonthlyMedian\evaluation_config.toml"])
 
 # results
 
--- README.md
+++ README.md
@@ -10,4 +10,4 @@ This repository contains all data used on the paper. The following list of Julia
 
 Folder /CompareScenariosGenerators contains the Natural Inflow Energy observed data in file NIE.csv and its subfolders contains all models used and their respective results. Users should simply edit the file paths in file CompareScenariosGenerators.jl and in each evaluation_config.toml and then run the code in CompareScenariosGenerators.jl to reproduce the obtained results.
 
-Worksheets on the root folder contains the results summary and graphs that are in the paper.
+Workbooks on the root folder contains the results summary and graphs that are in the paper.
